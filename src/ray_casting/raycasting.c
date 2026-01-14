4921d2ff3703cedbef72b58f34d2594aacea2a1f
MadlenMin <i23.Minasyan.Madlen@etud.ufar.am>
2026-01-14 05:21:08 +0400

    calculate distance added project wall in process (1 st step)

--- src/ray_casting/raycasting.c
+++ src/ray_casting/raycasting.c
@@ -1,15 +1,21 @@
 #include "../../cub3d.h"
 
-
-
-
 void calculate_ray_direction(t_data *data, t_ray *ray, int x )
 {
 	ray->cameraX = 2 * x / (double)SCREEN_WIDTH - 1;
 	ray->ray_dir_x = data->dir_x + data->plane_x * ray->cameraX;
 	ray->ray_dir_y = data->dir_y + data->plane_y * ray->cameraX;
 }
-
+double calculate_perpendiculare_wall_distence(t_data *data, t_ray *ray)
+{
+	double wall_distence;
+	wall_distence = 0;
+	if(ray->side == 0)
+		wall_distence = ray->side_dist_x - ray->delta_dist_x;
+	else
+		wall_distence = ray->side_dist_y - ray->delta_dist_y;
+	return (wall_distence);
+}
 void init_dda(t_data *data, t_ray *ray)
 {
 	if(ray->ray_dir_x == 0)
@@ -69,114 +75,33 @@ void perform_dda(t_data *data, t_ray *ray)
 	}
 
 }
-/*void project_wall(t_data *data, t_ray *ray, int colum)
+void project_wall(t_data *data, t_ray *ray, int colum, int wall_distence)
 {
+	double draw_start;
+	double draw_end;
+	double wall_hight;
 
-}*/
-static void debug_perform_dda(t_data *data, t_ray *ray)
+	wall_hight = SCREEN_HEIGHT/wall_distence;
+
+	draw_start = (SCREEN_HEIGHT / 2) - (wall_hight / 2);
+	draw_end = (SCREEN_HEIGHT / 2) + (wall_hight / 2);
+}
+
+void raycasting(t_data *data)
 {
-	int step_count = 0;
+	int colum;
+	t_ray ray;
+	double wall_distence;
 
-    printf("=== DEBUG DDA START ===\n");
-    printf("Ray dir: (%.3f, %.3f)\n", ray->ray_dir_x, ray->ray_dir_y);
-    printf("Player pos: (%.3f, %.3f)\n", data->pos_x, data->pos_y);
-    printf("Start cell: (%d, %d)\n\n", ray->map_x, ray->map_y);
-
-	while(!ray->hit)
+	wall_distence = 0.0;
+	colum = 0;
+	while (colum < SCREEN_WIDTH)
 	{
-		printf("Step %d\n", step_count);
-        printf("  Cell: (%d, %d)\n", ray->map_x, ray->map_y);
-        printf("  side_dist_x: %.3f | side_dist_y: %.3f\n",
-               ray->side_dist_x, ray->side_dist_y);
-
-			   if(ray->side_dist_x < ray->side_dist_y)
-		{
-			ray->side_dist_x += ray->delta_dist_x;
-			ray->map_x += ray->step_x;
-			ray->side = 0;
-			 printf("  -> Step in X direction\n");
-		}
-		else
-		{
-			ray->side_dist_y += ray->delta_dist_y;
-			ray->map_y += ray->step_y;
-			ray->side = 1;
-			 printf("  -> Step in Y direction\n");
-		}
-		if(data->map[ray->map_x][ray->map_y] == '1')
-		{
-			ray->hit = 1;
-			printf("  HIT WALL at (%d, %d)\n", ray->map_x, ray->map_y);
-            printf("  Hit side: %s\n", ray->side == 0 ? "VERTICAL (X)" : "HORIZONTAL (Y)");
-			//break ; 
-		}
-		    step_count++;
-			if (step_count > 100)
-        {
-            printf("ERROR: DDA did not terminate!\n");
-            break;
-        }
-
-        printf("\n");
+		calculate_ray_direction(data, &ray, colum);
+		init_dda(data, &ray);
+		perform_dda(data, &ray);
+		wall_distence = calculate_perpendicular_wall_distence(data, &ray);
+		project_wall(data, &ray, colum, wall_distence);
+		colum++;
 	}
-	  printf("=== DEBUG DDA END ===\n\n");
-
 }
-void debug_ray_dda(t_data *data, t_ray *ray)
-{
-	int step_count = 0;
-
-	while (!ray->hit)
-    {
-		
-		if (ray->side_dist_x < ray->side_dist_y)
-        {
-			ray->side_dist_x += ray->delta_dist_x;
-            ray->map_x += ray->step_x;
-            ray->side = 0;
-            printf("  -> Step in X direction\n");
-        }
-        else
-        {
-            ray->side_dist_y += ray->delta_dist_y;
-            ray->map_y += ray->step_y;
-            ray->side = 1;
-            printf("  -> Step in Y direction\n");
-        }
-		
-        if (data->map[ray->map_x][ray->map_y] == '1')
-        {
-            ray->hit = 1;
-            printf("  HIT WALL at (%d, %d)\n", ray->map_x, ray->map_y);
-            printf("  Hit side: %s\n", ray->side == 0 ? "VERTICAL (X)" : "HORIZONTAL (Y)");
-        }
-		
-        step_count++;
-        if (step_count > 100)
-        {
-            printf("ERROR: DDA did not terminate!\n");
-            break;
-        }
-
-        printf("\n");
-    }
-
-    printf("=== DEBUG DDA END ===\n\n");
-}
-		void raycasting(t_data *data)
-		{
-			int colum;
-			t_ray ray;
-		
-			colum = 0;
-			while (colum < SCREEN_WIDTH)
-			{
-				calculate_ray_direction(data, &ray, colum);
-				init_dda(data, &ray);
-				//perform_dda(data, &ray);
-				debug_perform_dda(data, &ray);
-				//calculate_wall_distence(data, &ray);
-				//project_wall(data, &ray, colum);
-				colum++;
-			}
-		}
